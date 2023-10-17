9db263765a7fbf3a1df43856fd73ada07395adb1
Jangcccci <jangcccci@gmail.com>
2023-10-17 22:00:02 +0900

    react-study-emotion-diary

---
+++ src/components/DiaryEditor.js
@@ -0,0 +1,138 @@
+import { useNavigate } from "react-router-dom";
+import {
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
+import { DiaryDispatchContext } from "./../App";
+
+import MyHeader from "./MyHeader";
+import MyButton from "./MyButton";
+import EmotionItem from "./EmotionItem";
+
+import { getStringDate } from "../util/date";
+import { emotionList } from "../util/emotion";
+
+const DiaryEditor = ({ isEdit, originData }) => {
+  const contentRef = useRef();
+  const [emotion, setEmotion] = useState(3);
+  const [date, setDate] = useState(getStringDate(new Date()));
+  const [content, setContent] = useState("");
+
+  const navigate = useNavigate();
+  const handleClickEmote = useCallback((emotion) => {
+    setEmotion(emotion);
+  }, []);
+
+  const { onCreate, onEdit, onRemove } = useContext(DiaryDispatchContext);
+  const handelSubmit = () => {
+    if (content.length < 1) {
+      contentRef.current.focus();
+      return;
+    }
+
+    if (
+      !window.confirm(
+        isEdit ? "일기를 수정하시겠습니까?" : "새로운 일기를 작성하시겠습니까?"
+      )
+    ) {
+      return;
+    }
+
+    if (isEdit) {
+      onEdit(originData.id, date, content, emotion);
+    } else {
+      onCreate(date, content, emotion);
+    }
+    navigate("/", { replace: true });
+  };
+
+  useEffect(() => {
+    if (isEdit) {
+      setDate(getStringDate(new Date(parseInt(originData.date))));
+      setEmotion(originData.emotion);
+      setContent(originData.content);
+    }
+  }, [isEdit, originData]);
+
+  const handleRemove = () => {
+    if (window.confirm("정말 삭제하시겠습니까?")) {
+      onRemove(originData.id);
+      navigate("/", { replace: true });
+    }
+  };
+
+  return (
+    <div className="DiaryEditor">
+      <MyHeader
+        headText={isEdit ? "일기 수정하기" : "새 일기 쓰기"}
+        leftChild={
+          <MyButton text={"< 뒤로가기"} onClick={() => navigate(-1)} />
+        }
+        rightChild={
+          isEdit && (
+            <MyButton
+              text={"삭제하기"}
+              onClick={handleRemove}
+              type={"negative"}
+            />
+          )
+        }
+      />
+      <div>
+        <section>
+          <h4>오늘은 언제인가요?</h4>
+          <div className="input_box">
+            <input
+              className="input_date"
+              type="date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+            />
+          </div>
+        </section>
+        <section>
+          <h4>오늘의 감정</h4>
+          <div className="input_box emotion_list_wrapper">
+            {emotionList.map((it) => (
+              <EmotionItem
+                key={it.emotion_id}
+                {...it}
+                onClick={handleClickEmote}
+                isSelected={it.emotion_id === emotion}
+              />
+            ))}
+          </div>
+        </section>
+        <section>
+          <h4>오늘의 일기</h4>
+          <div className="input_box_text_wrapper">
+            <textarea
+              placeholder="오늘은 어땠나요?"
+              ref={contentRef}
+              onChange={(e) => setContent(e.target.value)}
+              defaultValue={content}
+            ></textarea>
+          </div>
+        </section>
+        <section>
+          <div className="control-box">
+            <MyButton text={"취소하기"} onClick={() => navigate(-1)} />
+            <MyButton
+              text={"작성완료"}
+              type={"positive"}
+              onClick={() => {
+                handelSubmit();
+              }}
+            />
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default DiaryEditor;
